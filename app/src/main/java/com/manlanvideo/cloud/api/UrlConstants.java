871d7282794b335e5958ec3ae699317dbd4f044f
gzhulee <gzhulee@163.com>
2020-03-26 14:34:55 +0800

    modify url

--- app/src/main/java/com/manlanvideo/cloud/api/UrlConstants.java
+++ app/src/main/java/com/manlanvideo/cloud/api/UrlConstants.java
@@ -1,5 +1,5 @@
 package com.manlanvideo.cloud.api;
 
 public final class UrlConstants {
-    public static final String URI_CLIP_LIST = "/clip/list";
+    public static final String URI_CLIP_LIST = "clip/list";
 }
